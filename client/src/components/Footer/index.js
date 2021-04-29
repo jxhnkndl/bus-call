fff12984488818dccff5ee36f3d81f8e6abb591a
jxhnkndl <jkroyston@gmail.com>
2021-04-29 14:15:01 -0400

    Create and edit basic static footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+// Import dependencies
+import React from 'react';
+import './index.scss';
+
+// Create and export Footer component
+export default function Footer() {
+  return (
+    <footer>
+      <p>Powered by jxhnkndl</p>
+    </footer>
+  );
+}

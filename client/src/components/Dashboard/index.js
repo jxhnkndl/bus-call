29db28ec61356cb7a171b45cb3d1f2a6a2009485
jxhnkndl <jkroyston@gmail.com>
2021-04-30 17:00:22 -0400

    Build out conditional rendering of card components once API response updates the dashboard's state

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -6,11 +6,25 @@ import CardBody from '../CardBody';
 import CycleControl from '../CycleControl';
 import Daysheet from '../Daysheet';
 import StageSchedule from '../StageSchedule';
+import API from '../../utils/API';
 import './index.scss';
 
 // Create and export dashboard page component
 export default function Dashboard() {
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetchGigs();
+  }, []);
+
+  const fetchGigs = async () => {
+    try {
+      const res = await API.getGigs();
+      setResults(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <motion.div
@@ -22,18 +36,22 @@ export default function Dashboard() {
     >
       <section>
         <CycleControl />
-        <div className="row">
-          <div className="col-12 col-md-6 mb-4">
-            <CardBody>
+        {results.length ? (
+          <div className="row">
+            <div className="col-12 col-md-6 mb-4">
+              <CardBody>
+                
+              </CardBody>
+            </div>
+            <div className="col-12 col-md-6 mb-4">
+              <CardBody>
 
-            </CardBody>
+              </CardBody>
+            </div>
           </div>
-          <div className="col-12 col-md-6 mb-4">
-            <CardBody>
-
-            </CardBody>
-          </div>
-        </div>
+        ) : (
+          <p className="h4">NOTHING SCHEDULED</p>
+        )}
       </section>
     </motion.div>
   );

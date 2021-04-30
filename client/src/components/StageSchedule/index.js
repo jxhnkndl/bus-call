93bfc630899d182f429d5b92fd2401c7619e011e
jxhnkndl <jkroyston@gmail.com>
2021-04-29 22:35:59 -0400

    Conditionally map schedule blocks as list items and render only if a schedule is present on the gig document

--- client/src/components/StageSchedule/index.js
+++ client/src/components/StageSchedule/index.js
@@ -1,65 +1,41 @@
 // Import dependencies
-import React from 'react';
+import React, { useContext } from 'react';
+import GigsContext from '../../utils/gigsContext';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import './index.scss';
 
 // Create and export reusable card component
-export default function StageSchedule(props) {
+export default function StageSchedule() {
+  const { gigs, selected } = useContext(GigsContext);
+
   return (
     <div className="info-container">
       <p className="dashboard-heading h5 mb-1">Daysheet</p>
-      <ListGroup variant="flush">
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">12:30 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">Load In</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">3:30 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">Sound Check</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">6:30 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">Doors</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">7:30 - 8:00 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">The Bigger Lights</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">8:15 - 8:45 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">Anarbor</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">9:00 - 9:45 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">This Providence</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">10:05 - 11:20 PM</div>
-            <div className="col-12 col-lg-7 daysheet-item">Cartel</div>
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item className="daysheet-item">
-          <div className="row">
-            <div className="col-12 col-lg-5 daysheet-time">2:00 AM</div>
-            <div className="col-12 col-lg-7 daysheet-item">BUS CALL</div>
-          </div>
-        </ListGroup.Item>
-        
-      </ListGroup>
+
+      {selected.schedule.length ? (
+        <ListGroup key="schedule" variant="flush">
+          {selected.schedule.map((block, index) => {
+            return (
+              <ListGroup.Item
+                key={`${index}-${block._id}`}
+                className="daysheet-item"
+              >
+                <div className="row">
+                  <div className="col-12 col-lg-5 daysheet-time">
+                    {block.timeString}
+                  </div>
+                  <div className="col-12 col-lg-7 daysheet-item">
+                    {block.event}
+                  </div>
+                </div>
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      ) : (
+        <p className="h2">NOTHING SCHEDULED</p>
+      )}
     </div>
   );
 }

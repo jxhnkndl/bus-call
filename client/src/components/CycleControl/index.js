58c9509b92a311f13db681a567464c353b2b83bb
jxhnkndl <jkroyston@gmail.com>
2021-04-30 16:52:08 -0400

    Create UI component for cycling through results

---
+++ client/src/components/CycleControl/index.js
@@ -0,0 +1,32 @@
+// Import dependencies
+import React from 'react';
+import CardBody from '../CardBody';
+
+// Create and export cycle control component
+export default function CycleControl() {
+  return (
+    <div className="row">
+      <div className="col-12 mb-4">
+        <CardBody>
+          <div className="row">
+            <div className="col-2 d-flex justify-content-start align-items-center">
+              <a href="#">
+                <span className="material-icons md-48 material-icons-outlined">
+                  arrow_back_ios
+                </span>
+              </a>
+            </div>
+            <div className="col-8"></div>
+            <div className="col-2 d-flex justify-content-end align-items-center">
+              <a href="#">
+                <span className="material-icons md-48 material-icons-outlined">
+                  arrow_forward_ios
+                </span>
+              </a>
+            </div>
+          </div>
+        </CardBody>
+      </div>
+    </div>
+  );
+}

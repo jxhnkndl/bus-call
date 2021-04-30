19f08368de4ca44fb9352cdcef2ff11faa5979e1
jxhnkndl <jkroyston@gmail.com>
2021-04-29 22:12:53 -0400

    Import gigs context and thread props into JSX return block

--- client/src/components/Daysheet/index.js
+++ client/src/components/Daysheet/index.js
@@ -1,5 +1,7 @@
 // Import dependencies
-import React from 'react';
+import React, { useContext } from 'react';
+import GigsContext from '../../utils/gigsContext';
+import dayjs from 'dayjs';
 import './index.scss';
 
 // Use environmental variables
@@ -7,37 +9,37 @@ require('dotenv').config();
 
 // Create and export reusable card component
 export default function Daysheet(props) {
+  const { gigs, selected, upNext } = useContext(GigsContext);
+
   return (
     <div className="info-container">
-
       <div className="card-item">
         <p className="h5 mb-1">Today is:</p>
-        <p className="h2">Wed. April 28, 2021</p>
+        <p className="h2">{dayjs(selected.date).format('dddd MMM. D, YYYY')}</p>
       </div>
 
       <div className="card-item">
         <p className="h5 mb-1">You are in:</p>
-        <p className="h2">Baltimore, Maryland</p>
+        <p className="h2">{`${selected.venue.city}, ${selected.venue.state}.`}</p>
       </div>
-      
+
       <div className="card-item">
         <p className="h5 mb-1">Tonight's show:</p>
-        <p className="h2">Bourbon Street Quarter</p>
+        <p className="h2">{selected.venue.name}</p>
         <div className="row">
           <div className="col-12 col-lg-6 my-2 my-lg-0">
-            <p className="h5 mb-1">316 Guilford Avenue</p>
-            <p className="h5 mb-1">Baltimore, MD 21202</p>
-            <p className="h5 mb-1">(410) 528-8377</p>
+            <p className="h5 mb-1">{selected.venue.street}</p>
+            <p className="h5 mb-1">{`${selected.venue.city}, ${selected.venue.state}.`}</p>
           </div>
-          
+
           <div className="col-12 col-lg-6 my-2 my-lg-0">
             <p className="h5 mb-1">
               <span className="inline-heading">Capacity: </span>
-              450
+              {selected.venue.capacity}
             </p>
             <p className="h5 mb-1">
               <span className="inline-heading">Presale: </span>
-              388
+              {selected.presale}
             </p>
           </div>
         </div>
@@ -48,32 +50,36 @@ export default function Daysheet(props) {
           <div className="col-12 col-lg-6 my-2 my-lg-0">
             <p className="h5 mb-1">
               <span className="inline-heading">Catering: </span>
-              Buyout
+              {selected.catering}
             </p>
             <p className="h5 mb-1">
               <span className="inline-heading">Internet: </span>
-              Yes
+              {selected.internet ? 'Yes' : 'No'}
             </p>
           </div>
           <div className="col-12 col-lg-6 my-2 my-lg-0">
             <p className="h5 mb-1">
-              <span className="inline-heading">Dressing Rooms: </span>
-              2
+              <span className="inline-heading">Green Rooms: </span>
+              {selected.greenrooms}
             </p>
             <p className="h5 mb-1">
               <span className="inline-heading">Showers: </span>
-              Yes
+              {selected.showers ? 'Yes' : 'No'}
             </p>
           </div>
         </div>
       </div>
 
-      <div className="card-item">
-        <p className="h5 mb-1">Tomorrow:</p>
-        <p className="h4 mb-1">Wed. April 28, 2021</p>
-        <p className="h5 mb-1">South Hackensack, New Jersey</p>
-        <p className="h5 mb-1">@ School of Rock</p>
-      </div>
+      {upNext && (
+        <div className="card-item">
+          <p className="h5 mb-1">Tomorrow:</p>
+          <p className="h4 mb-1">
+            {dayjs(upNext.date).format('dddd MMM. D, YYYY')}
+          </p>
+          <p className="h5 mb-1">{`${upNext.venue.city}, ${upNext.venue.state}.`}</p>
+          <p className="h5 mb-1">{`@ ${upNext.venue.name}`}</p>
+        </div>
+      )}
     </div>
   );
 }

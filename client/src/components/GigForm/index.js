04810731175038340c4f4532a3858da0f77d6a25
jxhnkndl <jkroyston@gmail.com>
2021-04-30 20:52:37 -0400

    Begin creating input change and submit handlers for capturing form inputs and creating a POST request to API

--- client/src/components/GigForm/index.js
+++ client/src/components/GigForm/index.js
@@ -1,7 +1,8 @@
 // Import dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { pageVariants, pageTransitions } from '../../utils/transitions';
+import { emptyFormObj } from '../../utils/emptyFormObj';
 import CardBody from '../CardBody';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -9,6 +10,23 @@ import './index.scss';
 
 // Create and export dashboard page component
 export default function GigForm() {
+  const [formObj, setFormObj] = useState(emptyFormObj);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormObj({
+      ...formObj,
+      [name]: value,
+    });
+    console.log(formObj);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formObj);
+    setFormObj(emptyFormObj);
+  };
+
   return (
     <motion.div
       initial="initial"
@@ -26,31 +44,62 @@ export default function GigForm() {
                   <div className="col-12">
                     <Form.Group controlId="formGroupVenue">
                       <Form.Label>Venue Name:</Form.Label>
-                      <Form.Control type="text" placeholder="Enter Venue" />
+                      <Form.Control
+                        name="name"
+                        type="text"
+                        placeholder="Enter Venue"
+                        value={formObj.name}
+                        onChange={handleInputChange}
+                      />
                     </Form.Group>
                   </div>
                   <div className="col-6">
                     <Form.Group controlId="formGroupCity">
                       <Form.Label>City:</Form.Label>
-                      <Form.Control type="text" placeholder="Enter City" />
+                      <Form.Control
+                        name="city"
+                        type="text"
+                        placeholder="Enter City"
+                        value={formObj.city}
+                        onChange={handleInputChange}
+                      />
                     </Form.Group>
                   </div>
                   <div className="col-6">
                     <Form.Group controlId="formGroupState">
                       <Form.Label>City:</Form.Label>
-                      <Form.Control type="text" placeholder="Enter State" />
+                      <Form.Control
+                        name="state"
+                        type="text"
+                        placeholder="Enter State"
+                        value={formObj.state}
+                        onChange={handleInputChange}
+                      />
                     </Form.Group>
                   </div>
                   <div className="col-6">
                     <Form.Group controlId="formGroupState">
                       <Form.Label>Date:</Form.Label>
-                      <Form.Control type="date" placeholder="Enter State" />
+                      <Form.Control
+                        name="date"
+                        type="date"
+                        placeholder="Enter State"
+                        value={formObj.date}
+                        onChange={handleInputChange}
+                      />
                     </Form.Group>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-12">
-                    <Button variant="primary" className="py-2 px-3">Add Gig</Button>
+                    <Button
+                      type="submit"
+                      variant="primary"
+                      className="py-2 px-3"
+                      onClick={handleSubmit}
+                    >
+                      Add Gig
+                    </Button>
                   </div>
                 </div>
               </Form>

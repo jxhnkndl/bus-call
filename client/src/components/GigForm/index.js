06dd72a06f78cc2b8b163b5f970a1be4e8e9f4da
jxhnkndl <jkroyston@gmail.com>
2021-04-30 20:23:53 -0400

    Add minimal amount of form components to create and test interaction with API

--- client/src/components/GigForm/index.js
+++ client/src/components/GigForm/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { pageVariants, pageTransitions } from '../../utils/transitions';
 import CardBody from '../CardBody';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './index.scss';
 
 // Create and export dashboard page component
@@ -17,14 +19,41 @@ export default function GigForm() {
     >
       <section>
         <div className="row">
-          <div className="col-12 col-md-6 mb-4">
+          <div className="col-12 col-md-10 col-lg-8 mx-auto">
             <CardBody>
-              <h2>INPUT FIELDS</h2>
-            </CardBody>
-          </div>
-          <div className="col-12 col-md-6 mb-4">
-            <CardBody>
-              <h2>INPUT FIELDS</h2>
+              <Form>
+                <div className="row">
+                  <div className="col-12">
+                    <Form.Group controlId="formGroupVenue">
+                      <Form.Label>Venue Name:</Form.Label>
+                      <Form.Control type="text" placeholder="Enter Venue" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-6">
+                    <Form.Group controlId="formGroupCity">
+                      <Form.Label>City:</Form.Label>
+                      <Form.Control type="text" placeholder="Enter City" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-6">
+                    <Form.Group controlId="formGroupState">
+                      <Form.Label>City:</Form.Label>
+                      <Form.Control type="text" placeholder="Enter State" />
+                    </Form.Group>
+                  </div>
+                  <div className="col-6">
+                    <Form.Group controlId="formGroupState">
+                      <Form.Label>Date:</Form.Label>
+                      <Form.Control type="date" placeholder="Enter State" />
+                    </Form.Group>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12">
+                    <Button variant="primary" className="py-2 px-3">Add Gig</Button>
+                  </div>
+                </div>
+              </Form>
             </CardBody>
           </div>
         </div>

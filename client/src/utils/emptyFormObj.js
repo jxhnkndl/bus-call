81894696f40501dbaa4bfb334abc44a6e2f62520
jxhnkndl <jkroyston@gmail.com>
2021-05-01 11:26:31 -0400

    Update structure of empty form object to reflect updates made to the gigs model

--- client/src/utils/emptyFormObj.js
+++ client/src/utils/emptyFormObj.js
@@ -3,8 +3,17 @@ export const emptyFormObj = {
   date: '',
   venue: {
     name: '',
+    street: '',
     city: '',
     state: '',
-  }
+    zip: '',
+    capacity: '0',
+    presale: '0',
+  },
+  catering: false,
+  internet: false,
+  showers: false,
+  greenrooms: false,
+  schedule: []
 }
 

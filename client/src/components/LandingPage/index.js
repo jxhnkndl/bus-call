ef559e85370696ac9004d8c51b07692e61d17190
jxhnkndl <jkroyston@gmail.com>
2021-05-03 14:47:36 -0400

    Add placeholder landing page for testing user authentication in a deployed production build

---
+++ client/src/components/LandingPage/index.js
@@ -0,0 +1,16 @@
+// Import dependencies
+import React from 'react';
+import { pageVariants, pageTransitions } from '../../utils/transitions';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import CardBody from '../CardBody';
+import './index.scss';
+
+
+// Create and export LandingPage component
+export default function LandingPage() {
+  return (
+    <section className="container text-center">
+      <h1>BUS CALL LANDING PAGE</h1>
+    </section>
+  );
+}

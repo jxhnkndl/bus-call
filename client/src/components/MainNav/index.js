f0fd48a1fc5f1e0f9b4f80079de4cb2935212c68
jxhnkndl <jkroyston@gmail.com>
2021-05-03 14:46:07 -0400

    Add breakpoint for expanding Navbar insert conditionally rendered login and logout buttons for testing

--- client/src/components/MainNav/index.js
+++ client/src/components/MainNav/index.js
@@ -1,19 +1,32 @@
 // Import dependencies
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import LoginBtn from '../LoginBtn';
+import LogoutBtn from '../LogoutBtn';
 import './index.scss';
 
 // Create and export MainNav component
 export default function MainNav() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <Navbar variant="dark" className="mb-4">
+    <Navbar variant="dark" expand="lg" className="mb-4">
       <Navbar.Brand href="/">Bus Call</Navbar.Brand>
-      <Nav className="mr-auto">
-        <NavLink className="nav-link" exact to="/">Dashboard</NavLink>
-        <NavLink className="nav-link" to="/create-gig">Add Gig</NavLink>
-      </Nav>
+      <Navbar.Toggle aria-controls="nav-drawer" />
+      <Navbar.Collapse id="nav-drawer">
+        <Nav className="mr-auto">
+          <NavLink className="nav-link" exact to="/dashboard">
+            Dashboard
+          </NavLink>
+          <NavLink className="nav-link" exact to="/create-gig">
+            Add Gig
+          </NavLink>
+        </Nav>
+        <Nav>{isAuthenticated ? <LogoutBtn /> : <LoginBtn />}</Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }

9ddbf22ccfa17b208eaf48a0581f1576b2929efd
jxhnkndl <jkroyston@gmail.com>
2021-05-03 19:46:57 -0400

    Create dashboard view component for accessing tour daysheets and stage schedules

---
+++ client/src/components/DayView/index.js
@@ -0,0 +1,54 @@
+// Import dependencies
+import React from 'react';
+import { motion } from 'framer-motion';
+import { pageVariants, pageTransitions } from '../../utils/transitions';
+import CardBody from '../CardBody';
+import CycleControl from '../DashboardNav';
+import Daysheet from '../Daysheet';
+import StageSchedule from '../StageSchedule';
+import './index.scss';
+
+// Create and export dashboard page component
+export default function DayView(props) {
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransitions}
+    >
+      <div>
+        <div className="row">
+          <div className="col-12 col-md-6 mb-4">
+            <CardBody spacing={'p-4 p-md-4 m-2 m-md-0'}>
+              <Daysheet
+                date={props.date}
+                city={props.city}
+                state={props.state}
+                venue={props.venue}
+                street={props.street}
+                zip={props.zip}
+                capacity={props.capacity}
+                presale={props.presale}
+                greenrooms={props.greenrooms}
+                catering={props.catering}
+                showers={props.showers}
+                internet={props.internet}
+                nextDate={props.nextDate}
+                nextCity={props.nextCity}
+                nextState={props.nextState}
+                nextVenue={props.nextVenue}
+              />
+            </CardBody>
+          </div>
+          <div className="col-12 col-md-6 mb-4">
+            <CardBody spacing={'p-4 p-md-4 m-2 m-md-0'}>
+              <StageSchedule schedule={props.schedule} />
+            </CardBody>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}

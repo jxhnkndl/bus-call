b2a1d95bca2cb139461b2f33593207ae69d1b114
jxhnkndl <jkroyston@gmail.com>
2021-05-03 19:45:21 -0400

    Convert dashboard component into parent component with shared state serving daysheet and stage schedule view, routing view, and form view

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -2,26 +2,30 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { pageVariants, pageTransitions } from '../../utils/transitions';
+import Button from 'react-bootstrap/Button';
 import CardBody from '../CardBody';
-import CycleControl from '../CycleControl';
-import Daysheet from '../Daysheet';
-import StageSchedule from '../StageSchedule';
+import DashboardNav from '../DashboardNav';
+import DayView from '../DayView';
+import FormView from '../FormView';
 import API from '../../utils/API';
-import './index.scss';
 import dayjs from 'dayjs';
+import './index.scss';
 
 // Create and export dashboard page component
 export default function Dashboard() {
+  // Set initial state
   const [gigs, setGigs] = useState([]);
-  let [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [view, setView] = useState('day');
 
-  // On load
+  // Fetch gigs from API when component mounts and whenever the view state changes
   useEffect(() => {
     fetchGigs();
-  }, []);
+  }, [view]);
 
   // Fetch gigs from DB
   const fetchGigs = async () => {
+    console.log('fetching gigs...');
     try {
       const res = await API.getGigs();
       setGigs(res.data);
@@ -54,6 +58,11 @@ export default function Dashboard() {
     }
   };
 
+  // Click handler for change dashboard's view state
+  const handleView = (view) => {
+    setView(view);
+  }
+
   return (
     <motion.div
       initial="initial"
@@ -65,49 +74,39 @@ export default function Dashboard() {
       <section className="p-2 px-md-4 px-lg-5">
         {gigs.length ? (
           <div>
-            <CycleControl
-              spacing={'p-4 p-md-4 m-2 m-md-0'}
-              prev={handlePrev}
-              next={handleNext}
-            />
-            <div className="row">
-              <div className="col-12 col-md-6 mb-4">
-                <CardBody spacing={'p-4 p-md-4 m-2 m-md-0'}>
-                  <Daysheet
-                    date={handleDate(gigs[index].date)}
-                    city={gigs[index].venue.city}
-                    state={gigs[index].venue.state}
-                    venue={gigs[index].venue.name}
-                    street={gigs[index].venue.street}
-                    zip={gigs[index].venue.zip}
-                    capacity={gigs[index].venue.capacity}
-                    presale={gigs[index].venue.presale}
-                    greenrooms={gigs[index].greenrooms}
-                    catering={gigs[index].catering}
-                    showers={gigs[index].showers}
-                    internet={gigs[index].internet}
-                    nextDate={gigs[index + 1] ? gigs[index + 1].date : 'N/A'}
-                    nextCity={
-                      gigs[index + 1] ? gigs[index + 1].venue.city : 'N/A'
-                    }
-                    nextState={
-                      gigs[index + 1] ? gigs[index + 1].venue.state : 'N/A'
-                    }
-                    nextVenue={
-                      gigs[index + 1] ? gigs[index + 1].venue.name : 'N/A'
-                    }
-                  />
-                </CardBody>
-              </div>
-              <div className="col-12 col-md-6 mb-4">
-                <CardBody spacing={'p-4 p-md-4 m-2 m-md-0'}>
-                  <StageSchedule schedule={gigs[index].schedule} />
-                </CardBody>
-              </div>
-            </div>
+            {/* Dashboard navigation controls */}
+            <DashboardNav prev={handlePrev} next={handleNext} handleView={handleView} />
+
+            {/* Daysheet and stage schedule view */}
+            {view === 'day' && (
+              <DayView
+                date={handleDate(gigs[index].date)}
+                city={gigs[index].venue.city}
+                state={gigs[index].venue.state}
+                venue={gigs[index].venue.name}
+                street={gigs[index].venue.street}
+                zip={gigs[index].venue.zip}
+                capacity={gigs[index].venue.capacity}
+                presale={gigs[index].venue.presale}
+                greenrooms={gigs[index].greenrooms}
+                catering={gigs[index].catering}
+                showers={gigs[index].showers}
+                internet={gigs[index].internet}
+                nextDate={gigs[index + 1] ? gigs[index + 1].date : 'N/A'}
+                nextCity={gigs[index + 1] ? gigs[index + 1].venue.city : 'N/A'}
+                nextState={gigs[index + 1] ? gigs[index + 1].venue.state : 'N/A'}
+                nextVenue={gigs[index + 1] ? gigs[index + 1].venue.name : 'N/A'}
+                schedule={gigs[index].schedule}
+                prev={handlePrev}
+                next={handleNext}
+              />
+            )}
+
+            {/* Form view for adding and editing gigs */}
+            {view === 'form' && <FormView fetchGigs={fetchGigs} gigs={gigs} />}
           </div>
         ) : (
-          <p className="h4">NOTHING SCHEDULED</p>
+          <p className="h2">LOADING</p>
         )}
       </section>
     </motion.div>

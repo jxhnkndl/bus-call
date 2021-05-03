0f43f25a9396554337d9f7c70e806c2e5be1bbfc
jxhnkndl <jkroyston@gmail.com>
2021-05-03 19:46:06 -0400

    Convert cycle control component into a more functional and lightweight dashboard navigation component

---
+++ client/src/components/DashboardNav/index.js
@@ -0,0 +1,48 @@
+// Import dependencies
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import CardBody from '../CardBody';
+import Dropdown from 'react-bootstrap/Dropdown';
+import './index.scss';
+
+// Overwrite CardBody component styles for this component only
+const styles = {
+  cardBody: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+};
+
+// Create and export cycle control component
+export default function DashboardNav(props) {
+  console.log(props);
+
+  return (
+    <div className="row mb-2 dashboard-nav">
+      <div className="col-12">
+        <CardBody
+          flex={'d-flex align-items-center justify-content-between'}
+          spacing={'px-4 py-2 m-2 m-md-0'}
+        >
+          <Dropdown className="d-inline-block">
+            <Dropdown.Toggle id="dropdown-basic">View By</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onSelect={(e) => props.handleView('day')}>Daysheets</Dropdown.Item>
+              <Dropdown.Item onSelect={(e) => props.handleView('day')}>Routing</Dropdown.Item>
+              <Dropdown.Item onSelect={(e) => props.handleView('form')}>Add/Edit Gig</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <div>
+            <p className="nav-icon mb-0 mr-2" onClick={props.prev}>
+              <i className="fas fa-arrow-circle-left"></i>
+            </p>
+            <p className="nav-icon mb-0 ml-2" onClick={props.next}>
+              <i className="fas fa-arrow-circle-right"></i>
+            </p>
+          </div>
+        </CardBody>
+      </div>
+    </div>
+  );
+}

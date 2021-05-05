2ef31649b49e266691dcb4ad42d11e44f702ff7c
jxhnkndl <jkroyston@gmail.com>
2021-05-05 19:43:44 -0400

    Design and create landing page

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,15 +1,39 @@
 // Import dependencies
 import React from 'react';
 import { pageVariants, pageTransitions } from '../../utils/transitions';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from 'react-bootstrap/Button';
 import CardBody from '../CardBody';
 import './index.scss';
 
 // Create and export LandingPage component
 export default function LandingPage() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <header>
-      <h1 className="display-1 heading text-center mt-5">Touring made simple.</h1>
+    <header className="container">
+      <div className="row">
+        <div className="col-12 col-md-10 mx-auto">
+          <h1 className="headline">Never miss a beat</h1>
+          <h3 className="sub-headline">
+            <span className="d-block d-sm-inline-block mr-sm-2">
+              You play the gigs.
+            </span>
+            <span className="d-block d-sm-inline-block">
+              We'll handle the details.
+            </span>
+          </h3>
+          <hr />
+          <Button
+            variant="primary"
+            className="mt-3 px-4 py-2"
+            onClick={() => loginWithRedirect()}
+          >
+            <i className="fas fa-headphones-alt mr-2"></i>
+            Get Started
+          </Button>
+        </div>
+      </div>
     </header>
   );
 }
--- client/src/components/MainContent/index.js
+++ client/src/components/MainContent/index.js
@@ -5,8 +5,39 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LandingPage from '../LandingPage';
 import Dashboard from '../Dashboard';
 
+const bgImage = {
+  main: {
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/imgs/stage.jpg'})`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+    position: 'relative'
+  },
+  overlay: {
+    background: 'linear-gradient(0deg, rgba(33,33,33,1) 20%, rgba(118,118,118,0.3883928571428571) 100%)',
+    position: 'absolute',
+    minHeight: '100%',
+    minWidth: '100vw',
+    display: 'flex',
+    alignItems: 'center'
+  }
+};
+
 export default function MainContent() {
   const { isLoading, isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated && !isLoading) {
+    return (
+      <main style={bgImage.main} className="">
+        <div style={bgImage.overlay}>
+          <AnimatePresence exitBeforeEnter>
+            <LandingPage />
+          </AnimatePresence>
+        </div>
+      </main>
+    );
+  }
 
   if (isLoading) {
     return (
@@ -18,11 +49,13 @@ export default function MainContent() {
     );
   }
 
-  return (
-    <main style={{ overflow: 'hidden' }} className="">
-      <AnimatePresence exitBeforeEnter>
-        {isAuthenticated ? <Dashboard /> : <LandingPage />}
-      </AnimatePresence>
-    </main>
-  );
+  if (isAuthenticated) {
+    return (
+      <main style={{ overflow: 'hidden' }} className="">
+        <AnimatePresence exitBeforeEnter>
+          <Dashboard />
+        </AnimatePresence>
+      </main>
+    );
+  }
 }

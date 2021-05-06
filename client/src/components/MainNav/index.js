bc9fcd430cee494694aa3e8e7552354fddf50077
jxhnkndl <jkroyston@gmail.com>
2021-05-06 17:29:17 -0400

    Collapse user name, avatar, and logout button into a single dropdown button to improve responsiveness of navbar on mobile devices

--- client/src/components/MainNav/index.js
+++ client/src/components/MainNav/index.js
@@ -2,15 +2,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import LoginBtn from '../LoginBtn';
 import LogoutBtn from '../LogoutBtn';
 import './index.scss';
+import Dropdown from 'react-bootstrap/esm/Dropdown';
 
 // Create and export MainNav component
 export default function MainNav() {
-  const { isAuthenticated, user } = useAuth0();
+  const { isAuthenticated, user, logout } = useAuth0();
 
   console.log(user);
 
@@ -19,13 +21,18 @@ export default function MainNav() {
       <Navbar.Brand href="/">Bus Call</Navbar.Brand>
       <Nav className="ml-auto">
         {isAuthenticated ? (
-          <div className="media">
-            <img src={user.picture} className="avatar mr-3 rounded-circle" alt="Profile image" />
-            <div className="media-body mr-3">
-              <p className="h6 my-0">{`Hi ${user.given_name}!`}</p>
-            </div>
-            <LogoutBtn />
-          </div>
+          <DropdownButton variant="dark" menuAlign="right" title={<i class="fas fa-cog"></i>}>
+            <Dropdown.Item disabled="true">
+              <div>
+                <i className="fas fa-user-circle mr-2"></i>
+                {`Hi ${user.given_name}`}
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => logout()}>
+              <i className="fas fa-sign-out-alt mr-2"></i>
+              Logout
+            </Dropdown.Item>
+          </DropdownButton>
         ) : (
           <LoginBtn />
         )}

c1e433296a7beeaddd16c1818de20a88999cbaf9
jxhnkndl <jkroyston@gmail.com>
2021-05-08 13:27:09 -0400

    Update form objects to default to current tour's artist list when creating or editing gigs

--- client/src/utils/emptyFormObj.js
+++ client/src/utils/emptyFormObj.js
@@ -21,10 +21,10 @@ export const emptyFormObj = {
     { timeString: '12:00 PM', event: 'Load In' },
     { timeString: '3:00 PM', event: 'Soundcheck' },
     { timeString: '6:00 PM', event: 'Doors' },
-    { timeString: '7:00 - 7:30 PM', event: 'Opener' },
-    { timeString: '7:45 - 8:15 PM', event: 'Second' },
-    { timeString: '8:30 - 9:15 PM', event: 'Direct' },
-    { timeString: '9:30 - 10:50 PM', event: 'Headliner' },
+    { timeString: '7:00 - 7:30 PM', event: 'The Bigger Lights' },
+    { timeString: '7:45 - 8:15 PM', event: 'The Summer Set' },
+    { timeString: '8:30 - 9:15 PM', event: 'This Providence' },
+    { timeString: '9:30 - 10:50 PM', event: 'Cartel' },
     { timeString: '2:00 AM', event: 'Bus Call' },
   ]
 }
--- client/src/utils/formatObj.js
+++ client/src/utils/formatObj.js
@@ -24,10 +24,10 @@ export default function formatObj(obj) {
       { timeString: obj.schedule[0].timeString, event: "Load In" },
       { timeString: obj.schedule[1].timeString, event: "Sound Check" },
       { timeString: obj.schedule[2].timeString, event: "Doors" },
-      { timeString: obj.schedule[3].timeString, event: "Opener" },
-      { timeString: obj.schedule[4].timeString, event: "Second" },
-      { timeString: obj.schedule[5].timeString, event: "Direct" },
-      { timeString: obj.schedule[6].timeString, event: "Headliner" },
+      { timeString: obj.schedule[3].timeString, event: "The Bigger Lights" },
+      { timeString: obj.schedule[4].timeString, event: "The Summer Set" },
+      { timeString: obj.schedule[5].timeString, event: "This Providence" },
+      { timeString: obj.schedule[6].timeString, event: "Cartel" },
       { timeString: obj.schedule[7].timeString, event: "Bus Call" },
     ]
   }
